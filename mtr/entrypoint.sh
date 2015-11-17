04185a5f9a553c2b57d879ad8c3e92931e534cef
Marcus Martins <marcus.martins@docker.com>
2015-11-17 11:03:27 +0100

    Add a simple mtr container

---
+++ mtr/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+-x
+
+COUNT=100
+PACKET_SIZE=10000
+
+echo "Running for cloudfront"
+mtr --report dseasb33srnrn.cloudfront.net -c $COUNT -s $PACKET_SIZE --show-ips --tcp
+
+echo "Running for index.docker.io"
+mtr --report index.docker.io -c $COUNT -s $PACKET_SIZE --show-ips --tcp
